196556c8e49af66c402a2a397207eaede9642344
BlueTompon <lucastran00@gmail.com>
2021-12-17 00:35:28 +0100

    footer, responsive

--- docs/script.js
+++ docs/script.js
@@ -1,8 +1,34 @@
 const solution = document.querySelector('#solution');
 const solutionMenu = document.querySelector('.solutions-items');
-
+const body = document.querySelector('body');
 solution.addEventListener("click", solutionActionner);
 
 function solutionActionner(){
     solutionMenu.classList.toggle("solutions-items-active")
+    
 }
+
+const icon = document.querySelectorAll(".ionicon");
+let currentTheme = "Light";
+
+icon.forEach(el => {
+    el.addEventListener("click", themeActionner);
+});
+
+function themeActionner(event){
+    icon.forEach(el => {
+        el.classList.remove("theme-active")
+    });
+    if(event.currentTarget.id == "light"){
+        currentTheme = "dark";
+        icon[1].classList.add('theme-active');
+        body.setAttribute('theme','dark');
+    }
+    else {
+        currentTheme = "light";
+        icon[0].classList.add('theme-active');
+        body.setAttribute('theme','light');
+    }
+}
+
+
